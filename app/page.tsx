d72b7115647f1f4eb0c84971f5008117ff2eba82
amibiplob <103816819+Amibiplob@users.noreply.github.com>
2026-07-16 22:29:23 +0600

    dummy data added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/home/Banner";
+import FeaturedProducts from "@/components/home/featuredProducts";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <FeaturedProducts/>
     </>
   );
 }
